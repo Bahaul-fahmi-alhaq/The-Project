648950a8155350df3e5048183d37be7774dcb806
Bahaul-fahmi-alhaq <80311014+Bahaul-fahmi-alhaq@users.noreply.github.com>
2021-03-09 14:54:33 +0700

    Updating codes that is actually work

--- bilangan_prima_generator.py
+++ bilangan_prima_generator.py
@@ -1,24 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  5 12:59:04 2021
-
-@author: ACE
-"""
-
-#max_bil_prima = input()
-bil_prima = [2]
-x = 1
+import matplotlib.pyplot as plt
+max_bil_prima = 500 #limit of the prime number
+x = 0
 n = 1
 m = 0
-g = x/n
-while type(g) == float:    
-    n += 1
+bil_prima = []
+while x < max_bil_prima:
     g = x/n
-else:
-    m += 1
-    if m <= 2:
-        n +=1
-        g = x/n
-    elif n == x:
-        bil_prima.append(x)
-print('daftar bilangan prima', bil_prima)       
+    p = g-int(g)
+    while p == 0:
+        if x == n:
+            bil_prima.append(x)
+            x += 1
+            n = 1
+            m = 0
+            g = x/n
+            p = g-int(g)
+            continue
+        m += 1
+        if m<2:
+            n += 1
+            g = x/n
+            p = g-int(g)
+            continue
+        else:
+            x += 1
+            n = 1
+            m = 0
+            g = x/n
+            p = g-int(g)
+    n += 1
+    
+print(bil_prima)
+posisi = range(1,bil_prima[-1])
+plt.plot(posisi,posisi,,'g-', label='Bilangan Real')
